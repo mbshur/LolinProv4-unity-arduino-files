bb56ad984a9146cc2b2e6068a269d20d9105243b
Murat Hoca <mbshurricane@gmail.com>
2025-12-08 20:22:50 +0300

    1
    1

--- LolinproV4asset/Assets/FirebaseREST/FirebaseSettings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace FirebaseREST
-{
-    public class FirebaseSettings : MonoBehaviour
-    {
-        public string DATABASE_URL = "https://esp-v4-default-rtdb.firebaseio.com/";
-        public string WEB_API = "AIzaSyDTMhGs_ISD4WKmJrCxw35rqv-bo34ZdYI";
-    }
-}
